39dd46163e19102c46a7bac2403aa4b2ba96a8c6
m1ku100 <ilham.puji100@gmail.com>
2020-03-06 16:31:07 +0700

    Add DatabseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call([
+             KategoriSeed::class,
+             SubKategoriSeed::class,
+             ClusterKategoriSeed::class
+         ]);
     }
 }
