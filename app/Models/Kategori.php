5fb90bb094ab9fd6359b709ddb1b30bf97e765e1
m1ku100 <ilham.puji100@gmail.com>
2020-03-07 16:05:41 +0700

    Minor Update Model Kategri etc

--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+use League\Flysystem\Config;
 use Spatie\Translatable\HasTranslations;
 
 class Kategori extends Model
@@ -14,6 +16,15 @@ class Kategori extends Model
     protected $guarded = ['id'];
 
     public $translatable = ['name','caption'];
+
+    public function getLocale(): string
+    {
+        if (is_null(App::getLocale())) {
+            Config::set('app.locale', 'id');
+        }
+
+        return Config::get('app.locale');
+    }
 
     public function getSubKategori()
     {
--- app/Models/SubKategori.php
+++ app/Models/SubKategori.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+use League\Flysystem\Config;
 use Spatie\Translatable\HasTranslations;
 
 class SubKategori extends Model
@@ -14,6 +16,15 @@ class SubKategori extends Model
     protected $guarded = ['id'];
 
     public $translatable = ['name'];
+
+    public function getLocale(): string
+    {
+        if (is_null(App::getLocale())) {
+            Config::set('app.locale', 'id');
+        }
+
+        return Config::get('app.locale');
+    }
 
     public function getCluster()
     {
