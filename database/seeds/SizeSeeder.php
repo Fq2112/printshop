5559940963ecfe1f99e3881ac655d3c106866455
m1ku100 <ilham.puji100@gmail.com>
2020-04-14 21:19:12 +0700

    SizeSeeder done

---
+++ database/seeds/SizeSeeder.php
@@ -0,0 +1,417 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SizeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    const DATA = [
+        [
+            'name' => ['S ( 46 x 67 Cm)  ', 'S ( 46 x 67 Cm) '],
+            'description' => [
+                'S ( 46 x 67 Cm)',
+                'S ( 46 x 67 Cm) '
+            ],
+        ],
+        [
+            'name' => ['M ( 48 x 69 Cm)', 'M ( 48 x 69 Cm)'],
+            'description' => [
+                'M ( 48 x 69 Cm)',
+                'M ( 48 x 69 Cm)'
+            ],
+        ],
+        [
+            'name' => ['L ( 50 x 72 Cm)', 'L ( 50 x 72 Cm)'],
+            'description' => [
+                'L ( 50 x 72 Cm)',
+                'L ( 50 x 72 Cm)'
+            ],
+        ],
+        [
+            'name' => ['XL ( 53 x 74 Cm)', 'XL ( 53 x 7 4Cm)'],
+            'description' => [
+                'XL ( 53 x 74 Cm)',
+                'XL ( 53 x 74 Cm)'
+            ],
+        ],
+        [
+            'name' => ['XXL ( 55 x 76 Cm)', 'XXL ( 55 x 76 Cm)'],
+            'description' => [
+                'XXL ( 55 x 76 Cm)',
+                'XXL ( 55 x 76 Cm)'
+            ],
+        ],
+        [
+            'name' => ['9,0 x 5,5 cm', '9,0 x 5,5 cm'],
+            'description' => [
+                '9,0 x 5,5 cm',
+                '9,0 x 5,5 cm'
+            ],
+        ],
+        [
+            'name' => ['A4 (21 x 29,7 cm)', 'A4 (21 x 29,7 cm)'],
+            'description' => [
+                'A4 (21 x 29,7 cm)',
+                'F4 (21,6 x 33 cm)'
+            ],
+        ],
+        [
+            'name' => ['F4 (21,6 x 33 cm)', 'F4 (21,6 x 33 cm)'],
+            'description' => [
+                'F4 (21,6 x 33 cm)',
+                'F4 (21,6 x 33 cm)'
+            ],
+        ],
+        [
+            'name' => ['A5 (14,8 x 21 cm)', 'A5 (14,8 x 21 cm)'],
+            'description' => [
+                'A4 (21 x 29,7 cm)',
+                'F4 (21,6 x 33 cm)'
+            ],
+        ],
+        [
+            'name' => ['A6 (10.5 x 14.8 cm)', 'A6 (10.5 x 14.8 cm)'],
+            'description' => [
+                'A6 (10.5 x 14.8 cm)',
+                'A6 (10.5 x 14.8 cm)'
+            ],
+        ],
+        [
+            'name' => ['1.5 cm x 90 cm', '1.5 cm x 90 cm'],
+            'description' => [
+                '1.5 cm x 90 cm',
+                '1.5 cm x 90 cm'
+            ],
+        ],
+        [
+            'name' => ['2.5 cm x 90 cm', '2.5 cm x 90 cm'],
+            'description' => [
+                '2.5 cm x 90 cm',
+                '2.5 cm x 90 cm'
+            ],
+        ],
+        [
+            'name' => ['2 cm x 90 cm', '2 cm x 90 cm'],
+            'description' => [
+                '2 cm x 90 cm',
+                '2 cm x 90 cm'
+            ],
+        ],
+        [
+            'name' => ['12 Oz', '12 Oz'],
+            'description' => [
+                '12 Oz',
+                '12 Oz'
+            ],
+        ],
+        [
+            'name' => ['8 Oz', '8 Oz'],
+            'description' => [
+                '8 Oz',
+                '8 Oz'
+            ],
+        ],
+        [
+            'name' => ['18 Oz', '18 Oz'],
+            'description' => [
+                '18 Oz',
+                '18 Oz'
+            ],
+        ],
+        [
+            'name' => ['22 Oz', '22 Oz'],
+            'description' => [
+                '22 Oz',
+                '22 Oz'
+            ],
+        ],
+        [
+            'name' => ['FP Large (8 cm x 6.5 cm x 10.5 cm)', 'FP Large (8 cm x 6.5 cm x 10.5 cm)'],
+            'description' => [
+                'FP Large (8 cm x 6.5 cm x 10.5 cm)',
+                'FP Large (8 cm x 6.5 cm x 10.5 cm)'
+            ],
+        ],
+        [
+            'name' => ['16.2 cm x 21.6 cm x 5.5 cm (Sekat 3)', '16.2 cm x 21.6 cm x 5.5 cm (Sekat 3)'],
+            'description' => [
+                'Bento Lunch Box dengan 3 kompartemen',
+                'Bento Box With 3 Separate Sections'
+            ],
+        ],
+        [
+            'name' => ['16.2 cm x 21.6 cm x 5.5 cm (Sekat 4)', '16.2 cm x 21.6 cm x 5.5 cm (Sekat 4)'],
+            'description' => [
+                'Bento Lunch Box dengan 4 kompartemen',
+                'Bento Box With 4 Separate Sections'
+            ],
+        ],
+        [
+            'name' => ['S ( 16.5 x 8 x 20 cm)', 'S ( 16.5 x 8 x 20 cm)'],
+            'description' => [
+                'S ( 16.5 x 8 x 20 cm)',
+                'S ( 16.5 x 8 x 20 cm)'
+            ],
+        ],
+        [
+            'name' => ['M (32 cm x 8 cm x 27 cm)', 'M (32 cm x 8 cm x 27 cm)'],
+            'description' => [
+                'M (32 cm x 8 cm x 27 cm)',
+                'M (32 cm x 8 cm x 27 cm)'
+            ],
+        ],
+        [
+            'name' => ['L (40 cm x 12 cm x 30 cm)', 'L (40 cm x 12 cm x 30 cm)'],
+            'description' => [
+                'L (40 cm x 12 cm x 30 cm)',
+                'L (40 cm x 12 cm x 30 cm)'
+            ],
+        ],
+        [
+            'name' => ['17 cm x 10.5 cm x 6.5 cm', '17 cm x 10.5 cm x 6.5 cm'],
+            'description' => [
+                '17 cm x 10.5 cm x 6.5 cm',
+                '17 cm x 10.5 cm x 6.5 cm'
+            ],
+        ],
+        [
+            'name' => ['32 cm x 19 cm x 11 cm', '32 cm x 19 cm x 11 cm'],
+            'description' => [
+                '32 cm x 19 cm x 11 cm',
+                '32 cm x 19 cm x 11 cm'
+            ],
+        ],
+        [
+            'name' => ['S ( 22 x 12 x 11 Cm)', 'S ( 22 x 12 x 11 Cm)'],
+            'description' => [
+                'S ( 22 x 12 x 11 Cm)',
+                'S ( 22 x 12 x 11 Cm)'
+            ],
+        ],
+        [
+            'name' => ['M ( 36 x 23 x 19 Cm)', 'M ( 36 x 23 x 19 Cm)'],
+            'description' => [
+                'M ( 36 x 23 x 19 Cm))',
+                'M ( 36 x 23 x 19 Cm)'
+            ],
+        ],
+        [
+            'name' => ['L ( 45 x 27 x 30 Cm)', 'L ( 45 x 27 x 30 Cm)'],
+            'description' => [
+                'L ( 45 x 27 x 30 Cm)',
+                'L ( 45 x 27 x 30 Cm)'
+            ],
+        ],
+        [
+            'name' => ['Width : 2.4 cm , Length : 800 cm, Thickness : 48 micron', 'Width : 2.4 cm , Length : 800 cm, Thickness : 48 micron'],
+            'description' => [
+                'Width : 2.4 cm , Length : 800 cm, Thickness : 48 micron',
+                'Width : 2.4 cm , Length : 800 cm, Thickness : 48 micron'
+            ],
+        ],
+        [
+            'name' => ['Width : 4.8 cm , Length : 800 cm, Thickness : 48 micron', 'Width : 4.8 cm , Length : 800 cm, Thickness : 48 micron'],
+            'description' => [
+                'Width : 4.8 cm , Length : 800 cm, Thickness : 48 micron',
+                'Width : 4.8 cm , Length : 800 cm, Thickness : 48 micron'
+            ],
+        ],
+        [
+            'name' => ['Width : 4.8 cm , Length : 800 cm, Thickness : 48 micron', 'Width : 4.8 cm , Length : 800 cm, Thickness : 48 micron'],
+            'description' => [
+                'Width : 4.8 cm , Length : 800 cm, Thickness : 48 micron',
+                'Width : 4.8 cm , Length : 800 cm, Thickness : 48 micron'
+            ],
+        ],
+        [
+            'name' => ['3 cm x 5.5 cm', '3 cm x 5.5 cm'],
+            'description' => [
+                '3 cm x 5.5 cm',
+                '3 cm x 5.5 cm'
+            ],
+        ],
+        [
+            'name' => ['5 cm x 8 cm', '5 cm x 8 cm'],
+            'description' => [
+                '5 cm x 8 cm',
+                '5 cm x 8 cm'
+            ],
+        ],
+        [
+            'name' => ['12 cm x 7 cm x 17cm', '12 cm x 7 cm x 17cm'],
+            'description' => [
+                '12 cm x 7 cm x 17cm',
+                '12 cm x 7 cm x 17cm'
+            ],
+        ],
+        [
+            'name' => ['12 cm x 7 cm x 18.5 cm', '12 cm x 7 cm x 18.5 cm'],
+            'description' => [
+                '12 cm x 7 cm x 18.5 cm',
+                '12 cm x 7 cm x 18.5 cm'
+            ],
+        ],
+        [
+            'name' => ['12 cm x 7 cm x 23,5 cm', '12 cm x 7 cm x 23,5 cm'],
+            'description' => [
+                '12 cm x 7 cm x 23,5 cm',
+                '12 cm x 7 cm x 23,5 cm'
+            ],
+        ],
+        [
+            'name' => ['22.5 cm x 27 cm', '22.5 cm x 27 cm'],
+            'description' => [
+                '22.5 cm x 27 cm',
+                '22.5 cm x 27 cm'
+            ],
+        ],
+        [
+            'name' => ['30 cm x 30 cm', '30 cm x 30 cm'],
+            'description' => [
+                '30 cm x 30 cm',
+                '30 cm x 30 cm'
+            ],
+        ],
+        [
+            'name' => ['3.8 cm x 3.8 cm', '3.8 cm x 3.8 cm'],
+            'description' => [
+                '3.8 cm x 3.8 cm',
+                '3.8 cm x 3.8 cm'
+            ],
+        ],
+        [
+            'name' => ['6.4 cm x 6.4 cm', '6.4 cm x 6.4 cm'],
+            'description' => [
+                '6.4 cm x 6.4 cm',
+                '6.4 cm x 6.4 cm'
+            ],
+        ],
+        [
+            'name' => ['B2 (70 x 50 cm)', 'B2 (70 x 50 cm)'],
+            'description' => [
+                'B2 (70 x 50 cm)',
+                'B2 (70 x 50 cm)'
+            ],
+        ],
+        [
+            'name' => ['60 x 160 cm', '60 x 160 cm'],
+            'description' => [
+                '60 x 160 cm',
+                '60 x 160 cm'
+            ],
+        ],
+        [
+            'name' => ['80 cm x 180 cm', '80 cm x 180 cm'],
+            'description' => [
+                '80 cm x 180 cm',
+                '80 cm x 180 cm'
+            ],
+        ],
+        [
+            'name' => ['120 cm x 200 cm', '120 cm x 200 cm'],
+            'description' => [
+                '120 cm x 200 cm',
+                '120 cm x 200 cm'
+            ],
+        ],
+        [
+            'name' => ['14 x 6 cm', '14 x 6 cm'],
+            'description' => [
+                '14 x 6 cm',
+                '14 x 6 cm'
+            ],
+        ],
+        [
+            'name' => ['9 cm x 9 cm', '19 cm x 9 cm'],
+            'description' => [
+                '9 cm x 9 cm',
+                '9 cm x 9 cm'
+            ],
+        ],[
+            'name' => ['81 cm x 181 cm', '81 cm x 181 cm'],
+            'description' => [
+                '81 cm x 181 cm',
+                '81 cm x 181 cm'
+            ],
+        ],
+        [
+            'name' => ['8.5 cm x 5.5 cm', '8.5 cm x 5.5 cm'],
+            'description' => [
+                '8.5 cm x 5.5 cm',
+                '8.5 cm x 5.5 cm'
+            ],
+        ],
+        [
+            'name' => ['20 x 25 cm', '20 x 25 cm'],
+            'description' => [
+                '20 x 25 cm',
+                '20 x 25 cm'
+            ],
+        ],
+        [
+            'name' => ['30 x 40 cm', '30 x 40 cm'],
+            'description' => [
+                '30 x 40 cm',
+                '30 x 40 cm'
+            ],
+        ],
+        [
+            'name' => ['25 cm x 35 cm x 10 cm', '25 cm x 35 cm x 10 cm'],
+            'description' => [
+                '25 cm x 35 cm x 10 cm',
+                '25 cm x 35 cm x 10 cm'
+            ],
+        ],
+        [
+            'name' => ['30 x 40 x 10 cm', '30 x 40 x 10 cm'],
+            'description' => [
+                '30 x 40 x 10 cm',
+                '30 x 40 x 10 cm'
+            ],
+        ],
+        [
+            'name' => ['35 x 40 x 10 cm', '35 x 40 x 10 cm'],
+            'description' => [
+                '35 x 40 x 10 cm',
+                '35 x 40 x 10 cm'
+            ],
+        ],
+        [
+            'name' => ['Landscape (10.5 cm x 14.8 cm)', 'Landscape (10.5 cm x 14.8 cm)'],
+            'description' => [
+                'Bentuk hasil cetak memanjang ke samping.',
+                'Bentuk hasil cetak memanjang ke samping.'
+            ],
+        ],
+        [
+            'name' => ['Potrait (10.5 cm x 14.8 cm)', 'Potrait (10.5 cm x 14.8 cm)'],
+            'description' => [
+                'Bentuk hasil cetak memanjang ke atas.',
+                'Bentuk hasil cetak memanjang ke atas.'
+            ],
+        ],
+    ];
+
+    public function run()
+    {
+        foreach (self::DATA as $DATUM) {
+            $faker = \Faker\Factory::create('id_ID');
+            \App\Models\Size::create([
+                'name' => [
+                    'en' => $DATUM['name'][1],
+                    'id' => $DATUM['name'][0]
+                ],
+                'image' => $faker->imageUrl(),
+                'description' => [
+                    'en' => $DATUM['description'][1],
+                    'id' => $DATUM['description'][0]
+                ]
+            ]);
+        }
+    }
+}
