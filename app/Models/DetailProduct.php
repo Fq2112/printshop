eb4a45de5ef33d67f85d96890cc6af2ff4972266
m1ku100 <ilham.puji100@gmail.com>
2020-04-16 19:27:08 +0700

    Detail Cluster Data

---
+++ app/Models/DetailProduct.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DetailProduct extends Model
+{
+    protected $table = 'detail_products';
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'material_ids' => 'array',
+        'type_ids' => 'array',
+        'balance_ids' => 'array',
+        'page_ids' => 'array',
+        'copies_ids' => 'array',
+        'size_ids' => 'array',
+        'lamination_ids' => 'array',
+        'side_ids' => 'array',
+        'edge_ids' => 'array',
+        'color_ids' => 'array',
+        'front_side_ids' => 'array',
+        'back_side_ids' => 'array',
+        'right_side_ids' => 'array',
+        'left_side_ids' => 'array',
+        'front_cover_ids' => 'array',
+        'back_cover_ids' => 'array',
+        'binding_ids' => 'array',
+        'print_method_ids' => 'array',
+        'finishing_ids' => 'array',
+    ];
+}
