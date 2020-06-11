4ce4ad48a78e75e3cf94ab7bb1048eef4e1717c4
Fq2112 <fiqy_a@icloud.com>
2020-06-11 15:31:00 +0700

    header username too long (bug fixed)

--- resources/views/layouts/partials/_headerMenu.blade.php
+++ resources/views/layouts/partials/_headerMenu.blade.php
@@ -56,11 +56,12 @@
                     @if(Auth::check())
                         <img alt="Ava" class="img-thumbnail show_ava" src="{{Auth::user()->getBio->ava != "" ?
                         asset('storage/users/ava/'.Auth::user()->getBio->ava) : asset('admins/img/avatar/avatar-'.rand(1,5).'.png')}}">
-                        <b class="show_username">{{Auth::user()->username}}</b>
+                        <b class="show_username">{{\Illuminate\Support\Str::words(Auth::user()->username,18,'...')}}</b>
                     @elseif(Auth::guard('admin')->check())
                         <img alt="Ava" class="img-thumbnail show_ava" src="{{Auth::guard('admin')->user()->ava != "" ?
                         asset('storage/admins/ava/'.Auth::guard('admin')->user()->ava) :
-                        asset('admins/img/avatar/avatar-'.rand(1,5).'.png')}}">{{Auth::guard('admin')->user()->username}}
+                        asset('admins/img/avatar/avatar-'.rand(1,5).'.png')}}">
+                        {{\Illuminate\Support\Str::words(Auth::guard('admin')->user()->username,18,'...')}}
                     @endif
                 </div>
             </a>
