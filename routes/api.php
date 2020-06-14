d900df46b7dd7d4f979376ea2b7d3b55b7829778
m1ku100 <ilham.puji100@gmail.com>
2020-06-14 21:08:55 +0700

    Test CallBack Payment

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
+Route::group(['namespace' => 'API'], function () {
+
+    Route::get('test', [
+        'uses' => 'AcceptPaymentCallbackController@getCallback',
+        'as' => 'admin.dashboard'
+    ]);
+
+});
