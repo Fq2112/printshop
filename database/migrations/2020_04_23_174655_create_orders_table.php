d770fac8c4cc75ba461b36dc76324c89518fd571
m1ku100 <ilham.puji100@gmail.com>
2020-07-05 17:46:41 +0700

    Add Resi to each Order

--- database/migrations/2020_04_23_174655_create_orders_table.php
+++ database/migrations/2020_04_23_174655_create_orders_table.php
@@ -20,7 +20,7 @@ class CreateOrdersTable extends Migration
                 ->onDelete('CASCADE')->onUpdate('CASCADE');
             $table->string('progress_status');
             $table->string('uni_code')->unique();
-            $table->string('resi')->unique();
+            $table->string('resi')->nullable()->unique();
             $table->timestamps();
         });
     }
