4338bd6d5686cb8d24ebdaf6448c1ca8b1b509e6
Fq2112 <fiqy_a@icloud.com>
2020-10-01 16:30:38 +0700

    check specs if isActive false on form pemesanan (added)

--- resources/views/layouts/partials/users/_specsForm.blade.php
+++ resources/views/layouts/partials/users/_specsForm.blade.php
@@ -1,5 +1,5 @@
 <div class="panel-group" id="accordion" role="tablist">
-    @if($specs->is_type == true)
+    @if($specs->is_type == true && count(\App\Models\TypeProduct::whereIn('id', $specs->type_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-type">
                 <h4 class="panel-title">
@@ -60,7 +60,7 @@
         </div>
     @endif
 
-    @if($specs->is_material_cover == true)
+    @if($specs->is_material_cover == true && count(\App\Models\Material::whereIn('id', $specs->material_cover_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-cover_material">
                 <h4 class="panel-title">
@@ -120,7 +120,7 @@
         </div>
     @endif
 
-    @if($specs->is_side_cover == true)
+    @if($specs->is_side_cover == true && count(\App\Models\Side::whereIn('id', $specs->side_cover_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-cover_side">
                 <h4 class="panel-title">
@@ -182,7 +182,7 @@
         </div>
     @endif
 
-    @if($specs->is_cover_lamination == true)
+    @if($specs->is_cover_lamination == true && count(\App\Models\Lamination::whereIn('id', $specs->cover_lamination_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-cover_lamination">
                 <h4 class="panel-title">
@@ -246,7 +246,7 @@
         </div>
     @endif
 
-    @if($specs->is_material == true)
+    @if($specs->is_material == true && count(\App\Models\Material::whereIn('id', $specs->material_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-materials">
                 <h4 class="panel-title">
@@ -306,7 +306,7 @@
         </div>
     @endif
 
-    @if($specs->is_material_color == true)
+    @if($specs->is_material_color == true && count(\App\Models\Colors::whereIn('id', $specs->material_color_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-material_color">
                 <h4 class="panel-title">
@@ -388,7 +388,7 @@
         </div>
     @endif
 
-    @if($specs->is_color == true)
+    @if($specs->is_color == true && count(\App\Models\Colors::whereIn('id', $specs->color_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-color">
                 <h4 class="panel-title">
@@ -471,7 +471,7 @@
         </div>
     @endif
 
-    @if($specs->is_print_method == true)
+    @if($specs->is_print_method == true && count(\App\Models\PrintingMethods::whereIn('id', $specs->print_method_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-print_method">
                 <h4 class="panel-title">
@@ -535,7 +535,7 @@
         </div>
     @endif
 
-    @if($specs->is_size == true)
+    @if($specs->is_size == true && count(\App\Models\Size::whereIn('id', $specs->size_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-size">
                 <h4 class="panel-title">
@@ -617,7 +617,7 @@
         </div>
     @endif
 
-    @if($specs->is_side == true)
+    @if($specs->is_side == true && count(\App\Models\Side::whereIn('id', $specs->side_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-side">
                 <h4 class="panel-title">
@@ -679,7 +679,7 @@
         </div>
     @endif
 
-    @if($specs->is_holder == true)
+    @if($specs->is_holder == true && count(\App\Models\Finishing::whereIn('id', $specs->holder_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-holder">
                 <h4 class="panel-title">
@@ -741,7 +741,7 @@
         </div>
     @endif
 
-    @if($specs->is_lid == true)
+    @if($specs->is_lid == true && count(\App\Models\Lid::whereIn('id', $specs->lid_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-lid">
                 <h4 class="panel-title">
@@ -803,7 +803,7 @@
         </div>
     @endif
 
-    @if($specs->is_edge == true)
+    @if($specs->is_edge == true && count(\App\Models\Edge::whereIn('id', $specs->edge_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-corner">
                 <h4 class="panel-title">
@@ -865,7 +865,7 @@
         </div>
     @endif
 
-    @if($specs->is_folding == true)
+    @if($specs->is_folding == true && count(\App\Models\Folding::whereIn('id', $specs->folding_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-folding">
                 <h4 class="panel-title">
@@ -927,7 +927,7 @@
         </div>
     @endif
 
-    @if($specs->is_front_side == true)
+    @if($specs->is_front_side == true && count(\App\Models\Front::whereIn('id', $specs->front_side_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-front_side">
                 <h4 class="panel-title">
@@ -990,7 +990,7 @@
         </div>
     @endif
 
-    @if($specs->is_back_side == true)
+    @if($specs->is_back_side == true && count(\App\Models\BackSide::whereIn('id', $specs->back_side_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-back_side">
                 <h4 class="panel-title">
@@ -1053,7 +1053,7 @@
         </div>
     @endif
 
-    @if($specs->is_right_side == true)
+    @if($specs->is_right_side == true && count(\App\Models\RightLeftSide::whereIn('id', $specs->right_side_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-right_side">
                 <h4 class="panel-title">
@@ -1116,7 +1116,7 @@
         </div>
     @endif
 
-    @if($specs->is_left_side == true)
+    @if($specs->is_left_side == true && count(\App\Models\RightLeftSide::whereIn('id', $specs->left_side_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-left_side">
                 <h4 class="panel-title">
@@ -1179,7 +1179,7 @@
         </div>
     @endif
 
-    @if($specs->is_balance == true)
+    @if($specs->is_balance == true && count(\App\Models\Balance::whereIn('id', $specs->balance_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-balance">
                 <h4 class="panel-title">
@@ -1242,7 +1242,7 @@
         </div>
     @endif
 
-    @if($specs->is_copies == true)
+    @if($specs->is_copies == true && count(\App\Models\Copies::whereIn('id', $specs->copies_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-copies">
                 <h4 class="panel-title">
@@ -1305,7 +1305,7 @@
         </div>
     @endif
 
-    @if($specs->is_page == true)
+    @if($specs->is_page == true && count(\App\Models\Pages::whereIn('id', $specs->page_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-page">
                 <h4 class="panel-title">
@@ -1368,7 +1368,7 @@
         </div>
     @endif
 
-    @if($specs->is_front_cover == true)
+    @if($specs->is_front_cover == true && count(\App\Models\Material::whereIn('id', $specs->front_cover_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-front_cover">
                 <h4 class="panel-title">
@@ -1429,7 +1429,7 @@
         </div>
     @endif
 
-    @if($specs->is_back_cover == true)
+    @if($specs->is_back_cover == true && count(\App\Models\Material::whereIn('id', $specs->back_cover_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-back_cover">
                 <h4 class="panel-title">
@@ -1490,7 +1490,7 @@
         </div>
     @endif
 
-    @if($specs->is_orientation == true)
+    @if($specs->is_orientation == true && count(\App\Models\Finishing::whereIn('id', $specs->orientation_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-orientation">
                 <h4 class="panel-title">
@@ -1551,7 +1551,7 @@
         </div>
     @endif
 
-    @if($specs->is_binding == true)
+    @if($specs->is_binding == true && count(\App\Models\Finishing::whereIn('id', $specs->binding_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-binding">
                 <h4 class="panel-title">
@@ -1612,7 +1612,7 @@
         </div>
     @endif
 
-    @if($specs->is_lamination == true)
+    @if($specs->is_lamination == true && count(\App\Models\Lamination::whereIn('id', $specs->lamination_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-lamination">
                 <h4 class="panel-title">
@@ -1675,7 +1675,7 @@
         </div>
     @endif
 
-    @if($specs->is_finishing == true)
+    @if($specs->is_finishing == true && count(\App\Models\Finishing::whereIn('id', $specs->finishing_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-finishing">
                 <h4 class="panel-title">
@@ -1734,7 +1734,7 @@
         </div>
     @endif
 
-    @if($specs->is_extra == true)
+    @if($specs->is_extra == true && count(\App\Models\Finishing::whereIn('id', $specs->extra_ids)->where('isActive', true)->get()) > 0)
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" id="heading-extra">
                 <h4 class="panel-title">
