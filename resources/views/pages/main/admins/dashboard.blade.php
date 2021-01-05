786f95036c6f72d6389a5f182af6dc64dc7b5de7
Fq2112 <fiqy_a@icloud.com>
2021-01-05 09:15:38 +0700

    Admin Side: - dashboard icon

--- resources/views/pages/main/admins/dashboard.blade.php
+++ resources/views/pages/main/admins/dashboard.blade.php
@@ -110,7 +110,7 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-primary">
-                        <i class="fas fa-users"></i>
+                        <i class="fas fa-user-tie"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
@@ -140,7 +140,7 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-success">
-                        <i class="fas fa-tools"></i>
+                        <i class="fas fa-archive"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
